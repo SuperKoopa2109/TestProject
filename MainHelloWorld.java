d45ad3cef96fedb5f65a833d1293a137903da639
SuperKoopa2109 <48280110+SuperKoopa2109@users.noreply.github.com>
2019-06-14 15:32:48 +0200

    Update MainHelloWorld.java

--- MainHelloWorld.java
+++ MainHelloWorld.java
@@ -13,7 +13,15 @@ public class MainHelloWorld {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		System.out.println(Output(1));
+	}
+	
+	public static String Output(int varNumber){
+		if(varNumber == 0) {
+			return "Hello World";
+		} else {
+			return "Andere Eingabe";
+		}
 	}
 
 }
